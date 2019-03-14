a9f10eab0e190693e687030fc124baaea93ba24a
James Dizon <dizonjerlin@gmail.com>
2019-03-14 00:49:31 -0700

    FinalVersion

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gify_homework
+
+In this assignment, we are challenged to create a webpage for Gifs using JQuery, CSS, HTML and API keys.
+
+Issues:
+I had a hard time trying to get my buttons to work. And getting the Gifs to show.
+
--- app.js
+++ app.js
@@ -1,32 +1,73 @@
-$(document).ready(function(){
+var topics = ["Full House", "Grey's Anatomy", "How to Get Away with Murder", "One Tree Hill", "Gossip Girl", "The Good Doctor", "Pokemon", "Naruto"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG-13";
 
-let $input = $('#input');
-let $submit = $('#submit');
-let apiKey= "kADetQ01BAu11Qpipx9d9f6pVC3ksXPR"
-let $imgBody = $('.img-body')
-$submit.on('click', function(event){
-    event.preventDefault();
-    let inputVal = $input.val();
-    getGif(inputVal);
-});
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("tv-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".tv-button").unbind("click");
 
-function getGif(inputVal){
-    $get("http://api.giphy.com/v1/gifs/search?q=' + inputVal + '&api_key=" + apiKey + "&limit=10")
-    .done(function (data){
-        for(let i = 0; i <= 10; i++){
-            let gifImg = data.data[0].images.downsized.url);
-            createBox(gifImg);
-        }
-        
-    }); 
-};
+	$(".tv-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
 
-function createBox(gifImg) {
-    let $newImg = $('<img>');
-        $newImg.attr('src', )
-    let $newDiv =$('div');
-    
-    $imgBody.append($newImg)
 }
 
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
+		"&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&rating=" + cutOffRating + "&limit=" + numberOfGIFs,
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+		});
+		
+		$("#gif-container").addClass("dotted-border");
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#tv-show").val().trim());
+		$("#tv-show").val("");
+	});
 });
